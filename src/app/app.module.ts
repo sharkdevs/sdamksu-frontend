8dc413a74e7b34e0adc59fe3e393b5d6dccfef60
Meshack Ogeto <sharkdevs@Meshacks-MacBook-Pro.local>
2019-10-10 09:31:54 +0300

    ft(landingPage): create a landing page
    - create a navbar
    - add a landing section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './landing-page/nav-bar/nav-bar.component';
 import {
   MatAutocompleteModule,
   MatBadgeModule,
