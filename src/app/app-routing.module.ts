47c065863caad414a14a575487c2e7aeb0ed6402
Meshack Ogeto <sharkdevs@Meshacks-MacBook-Pro.local>
2019-10-14 15:16:59 +0300

    ft(modules): create dashboard and website modules
    - create website modules
    - create dashboard modules
    - refactor the components into modules
    
    [Finishes]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: WebsiteModule
+  // },
+  { path: '', loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule) },
+  { path: 'admin', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
